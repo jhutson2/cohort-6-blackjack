c779f26d8cb69debed523f145ddbcd88d0fe93ec
Michael Grein <michaelgrein92@gmail.com>
2016-11-28 12:17:16 -0500

    card values as hash

--- blackjack.rb
+++ blackjack.rb
@@ -19,23 +19,24 @@ class Deck
     @cards = []
 
     ["Spades", "Clubs", "Hearts", "Diamonds"].each do |suit|
-      [2,3,4,5,6,7,8,9,10].each do |value|
+      faces_and_values = {
+        2       => 2,
+        3       => 3,
+        4       => 4,
+        5       => 5,
+        6       => 6,
+        7       => 7,
+        8       => 8,
+        9       => 9,
+        10      => 10,
+        "Jack"  => 10,
+        "Queen" => 10,
+        "King"  => 10,
+        "Ace"   => 11
+      }
+      faces_and_values.each do |face, value|
         card = Card.new
         card.value = value
-        card.suit = suit
-        card.face = value
-        @cards << card
-      end
-      ["Jack", "Queen", "King"].each do |face|
-        card = Card.new
-        card.value = 10
-        card.suit = suit
-        card.face = face
-        @cards << card
-      end
-      ["Ace"].each do |face|
-        card = Card.new
-        card.value = 11
         card.suit = suit
         card.face = face
         @cards << card
